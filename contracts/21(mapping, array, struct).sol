2364df18006eb417ddb1d6ca71c1a3458b05b7f3
Remix Workspace <>
2022-10-27 13:20:46 +0800

    120

---
+++ contracts/21(mapping, array, struct).sol
@@ -0,0 +1,20 @@
+// 實作 mapping
+// 實作 array
+// 實作 struct
+
+
+mapping (address =>unit) public balance 
+
+constructor() public {
+    mapping_1[0] = 'Hello';
+    mapping_1[1] = 'World';
+    unit_array_1.push(10);
+    unit_array_1.push(21);
+    unit_array_2[0] = 45;
+    unit_array_2[2] = 70;
+    product_1 = struct_1(1, 199);
+
+    ab[0] = 5;
+    ab[1] = 10;
+
+} 
